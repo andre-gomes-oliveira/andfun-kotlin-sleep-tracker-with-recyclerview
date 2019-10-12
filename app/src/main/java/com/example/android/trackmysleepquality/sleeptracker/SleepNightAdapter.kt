d8ae8972e2c3418a722c49c17f6377a5d8ebf7db
André Gomes de Oliveira <andre.gomes.olivera@gmail.com>
2019-10-12 17:00:21 -0300

    Exercise-06 -  Refactoring the onCreateViewHOlder function to the ViewHolder

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -44,24 +44,30 @@ class SleepNightAdapter : RecyclerView.Adapter<SleepNightAdapter.ViewHolder>() {
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val layoutInflater = LayoutInflater.from(parent.context)
-        val view = layoutInflater
-                .inflate(R.layout.list_item_sleep_night, parent, false)
-
-        return ViewHolder(view)
+        return ViewHolder.from(parent)
     }
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
-        // TODO (01) Refactor the logic for creating the ViewHolder into a function called from().
+    class ViewHolder private constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        // DONE (01) Refactor the logic for creating the ViewHolder into a function called from().
 
-        // TODO (02) Make the from() function into a companion object.
+        // DONE (02) Make the from() function into a companion object.
 
-        // TODO (03) Move the companion object into the ViewHolder class, and have it
+        // DONE (03) Move the companion object into the ViewHolder class, and have it
         // return a ViewHolder.
 
         val sleepLength: TextView = itemView.findViewById(R.id.sleep_length)
         val quality: TextView = itemView.findViewById(R.id.quality_string)
         val qualityImage: ImageView = itemView.findViewById(R.id.quality_image)
+
+        companion object {
+            fun from(parent: ViewGroup): ViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val view = layoutInflater
+                        .inflate(R.layout.list_item_sleep_night, parent, false)
+
+                return ViewHolder(view)
+            }
+        }
 
         fun bind(item: SleepNight) {
             val res = itemView.context.resources
@@ -79,5 +85,4 @@ class SleepNightAdapter : RecyclerView.Adapter<SleepNightAdapter.ViewHolder>() {
             })
         }
     }
-
 }
